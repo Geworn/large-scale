8a659895363449107ac8b951b487ffbec04f6212
PatphonSD <muc5674@gmail.com>
2024-05-07 13:17:54 +0700

    created socket.io

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 import { SerialPort } from "serialport";
+import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "node:http";
 
-const serialport = new SerialPort({ path: "COM7", baudRate: 115200 });
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+const serialport = new SerialPort({ path: "/dev/ttyUSB0", baudRate: 115200 });
 
 async function main() {
-  while (true) {
-    const bufferInput = serialport.read();
-    await new Promise((reslove, reject) =>
-      setTimeout(() => {
-        reslove();
-      }, 250)
-    );
-    const weight = bufferInput ? parseFloat(Buffer.from(bufferInput).toString()) : 0.0;
-    console.log({ weight });
+  let weight = 0.0;
+  try {
+    serialport.on("data", (chunk) => {
+      const _weight = chunk ? parseFloat(Buffer.from(chunk).toString()) : 0.0;
+      weight = _weight;
+      console.log({ weight });
+
+    io.on("connection", (socket) => {
+        console.log("a user connected");
+        setInterval(() => {
+          socket.emit("weight", weight);
+        }, 100);
+      });
+    });
+
+    server.listen(3250, () => {
+      console.log("server running at http://localhost:3250");
+    });
+  } catch (error) {
+    console.error(error);
   }
 }
 

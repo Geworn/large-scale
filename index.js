dd31eaad8267074a37cf4c369cb1a4cb8426224f
PatphonSD <muc5674@gmail.com>
2024-05-07 12:37:40 +0700

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+import { SerialPort } from "serialport";
+
+const serialport = new SerialPort({ path: "/dev/ttyUSB0", baudRate: 115200 });
+
+async function main() {
+  while (true) {
+    const bufferInput = serialport.read();
+    await new Promise((reslove, reject) =>
+      setTimeout(() => {
+        reslove();
+      }, 250)
+    );
+    const weight = bufferInput ? parseFloat(Buffer.from(bufferInput).toString()) : 0.0;
+    console.log({ weight });
+  }
+}
+
+main();
